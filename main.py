0637ead8cb06d03281b4914edd48ca76c0d21935
Arya Sapkal <2024asap22@gmail.com>
2025-09-11 19:42:56 -0400

    Added basic visualizations for each task in a temporary 'tasks' array using HTML and Bootstrap

--- main.py
+++ main.py
@@ -21,10 +21,31 @@ app = Flask(__name__)
         c.execute(f"SELECT * FROM tasks WHERE id={my_task.db_id}")
         #print(f'Here they are:{c.fetchall()}')"""
 
+
+tasks = [
+    {
+        'id': '4',
+        'task_name': 'Study for the intro to chem quiz on Friday.',
+        'description': 'Study nomenclature, isotopes, dim analysis, and the basics.',
+        'start_time': str(datetime.datetime.now()),
+        'duration': datetime.timedelta(hours=2)
+    },
+    {
+        'id': '5',
+        'task_name': 'Work on the Battleship project.',
+        'description': 'Work on the 1st part of the project. Leave enough time to ask questions.',
+        'start_time': str(datetime.datetime.now()),
+        'duration': datetime.timedelta(hours=2)
+    }
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", tasks=tasks)
+
+
+
 
 
 
